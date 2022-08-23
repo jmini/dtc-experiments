6ec0f59d038a24ac7ba236c25d0553ddbbbbc9e2
jmini <jmini@users.noreply.github.com>
2022-08-23 11:51:24 +0000

    deploy: eb57018c2f59b1c53148895def25d3f2e7da6133

--- lunrjsindex.js
+++ lunrjsindex.js
@@ -2,22 +2,30 @@ var documents = [
 
 {
     "id": 0,
-    "uri": "10_unu/10_intro.html",
-    "menu": "unu",
-    "title": "Intro",
-    "text": " Table of Contents 1. Intro 1. Intro Basic ditaa block: Lorem lacinia pellentesque odio ac facilisis ipsum . "
+    "uri": "20_du/40_drawio.html",
+    "menu": "du",
+    "title": "Draw.io Example",
+    "text": " Table of Contents 1. Draw.io Example 1. Draw.io Example Pattern [file].dio.png to indicate that the file can be opened by Draw.io / Diagrams.net "
 },
 
 {
     "id": 1,
-    "uri": "30_tri/20_advanced/20_topic-B.html",
-    "menu": "tri",
-    "title": "Topic B",
-    "text": " Table of Contents 1. Topic B 1. Topic B Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. "
+    "uri": "20_du/20_asciidoc.html",
+    "menu": "du",
+    "title": "AsciiDoc",
+    "text": " Table of Contents 1. AsciiDoc 1.1. Lists 1.2. Bold, italic, and monospace formatting syntax 1.3. Admonitions 1.4. Tables 1.5. Replacements 1. AsciiDoc 1.1. Lists 1.1.1. Unordered list List item Nested list item Nested list item List item Another nested list item List item 1.1.2. Ordered list Step 1 Step 2 Step 2a Step 2b Step 3 1.1.3. Description list First term The description can be placed on the same line as the term. Second term Description of the second term. The description can also start on its own line. 1.2. Bold, italic, and monospace formatting syntax bold constrained &amp; un constrained italic constrained &amp; un constrained bold italic constrained &amp; un constrained monospace constrained &amp; un constrained monospace bold constrained &amp; un constrained monospace italic constrained &amp; un constrained monospace bold italic constrained &amp; un constrained 1.3. Admonitions Note An admonition draws the reader&#8217;s attention to auxiliary information. Here are the other built-in admonition types: Important Don&#8217;t forget the children! Tip Look for the warp zone under the bridge. Caution Slippery when wet. Warning The software you&#8217;re about to use is untested. Important Sign off before stepping away from your computer. 1.4. Tables Column 1, Header Row Column 2, Header Row Cell in column 1, row 1 Cell in column 2, row 1 Cell in column 1, row 2 Cell in column 2, row 2 1.4.1. With headers Name Group Firefox Web Browser Ruby Programming Language 1.5. Replacements Name Characters Result Copyright (C) &#169; Registered (R) &#174; Registered (R) &#174; Trademark (TM) &#8482; Em dash -- &#8201;&#8212;&#8201; Ellipsis ... &#8230;&#8203; Single right arrow -&gt; &#8594; Double right arrow =&gt; &#8658; Single left arrow &lt;- &#8592; Double left arrow &lt;= &#8656; "
 },
 
 {
     "id": 2,
+    "uri": "20_du/10_lorem.html",
+    "menu": "du",
+    "title": "Lorem",
+    "text": " Table of Contents 1. Lorem 1. Lorem Lorem diam sed, efficitur massa. Ipsum sed in risus rutrum. Sed lacinia finibus dui et lobortis. Pellentesque et enim. Etiam arcu odio, faucibus non eros nec, mattis tristique mauris. Cras eget aliquet nibh. Curabitur in porta sem, maximus aliquet diam. Integer vitae augue eget ex viverra pharetra. In eu risus at diam lacinia dignissim. Duis vel mauris sed ex lobortis dictum. Nunc sagittis felis et turpis bibendum, id aliquam libero fermentum. Ut dolor nisi, congue nec lorem ac, commodo bibendum nunc. Nam ac felis elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sapien orci, luctus nec ligula nec, ornare hendrerit nunc. Cras vitae consectetur turpis. Phasellus lacinia pellentesque odio ac facilisis Listing 1 . Phasellus aliquam ullamcorper ligula porttitor ullamcorper. Vestibulum lorem leo, mattis quis ligula eget, vulputate dignissim magna. Listing 1. Lorem JSON example { id: 364528, lorem: lorem, data: [9786e2c1, c51147af, 81aa2d6d, 6a5a6b82] } Curabitur in diam quam. Cras eu purus lorem. Phasellus ut blandit ex. Morbi maximus eleifend enim, in venenatis enim auctor pharetra. Sed congue mollis tellus, id molestie libero dictum vitae. Aliquam erat volutpat. Quisque nec blandit nulla. Morbi pretium quis lacus sed fermentum. Donec convallis quam non odio maximus, sed mollis lacus mollis. Sed vehicula auctor magna at dignissim. Donec quis urna et est placerat malesuada. Donec fringilla magna id orci vulputate, eget venenatis ex posuere. Quisque malesuada et mi non finibus. Proin sit amet viverra lorem. Nulla ipsum eros, aliquam id felis sed, malesuada faucibus nisi. Nam pulvinar, sem non tristique scelerisque, mi orci tincidunt justo, quis interdum odio libero at lorem. In luctus iaculis risus, sit amet aliquet metus rutrum molestie. Fusce nec aliquet tellus. Aliquam tincidunt purus dictum quam sodales tempor. Quisque tincidunt purus sem, quis efficitur justo pharetra non. "
+},
+
+{
+    "id": 3,
     "uri": "30_tri/20_advanced/10_topic-A.html",
     "menu": "tri",
     "title": "Topic A",
@@ -25,19 +33,11 @@ var documents = [
 },
 
 {
-    "id": 3,
+    "id": 4,
     "uri": "30_tri/10_ipsum.html",
     "menu": "tri",
     "title": "Ipsum",
     "text": " Table of Contents 1. Ipsum 1. Ipsum Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. Aenean pulvinar sapien ex, id ultrices lacus consectetur ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et euismod est. Integer eleifend ornare accumsan. Etiam non nulla ut urna suscipit bibendum sed sed urna. Aenean maximus dictum venenatis. Nullam ultrices lorem sed rutrum egestas. Duis id felis at ex congue venenatis at non turpis. Praesent quis molestie justo. Nam a elementum elit. Nunc vehicula fermentum euismod. Etiam fermentum et purus sed interdum. Phasellus aliquam ullamcorper ligula porttitor ullamcorper. Vestibulum lorem leo, mattis quis ligula eget, vulputate dignissim magna. Listing 1. Ipsum properties example ipsum.enabled=true ipsum.name=ipsum ipsum.id=12598431 Morbi pretium quis lacus sed fermentum. Donec convallis quam non odio maximus, sed mollis lacus mollis. Sed vehicula auctor magna at dignissim. Donec quis urna et est placerat malesuada. Donec fringilla magna id orci vulputate, eget venenatis ex posuere. Quisque malesuada et mi non finibus lorem . Curabitur in diam quam. Cras eu purus lorem. Phasellus ut blandit ex. Morbi maximus eleifend enim, in venenatis enim auctor pharetra. Sed congue mollis tellus, id molestie libero dictum vitae. Aliquam erat volutpat. Quisque nec blandit nulla. "
-},
-
-{
-    "id": 4,
-    "uri": "10_unu/20_content.html",
-    "menu": "unu",
-    "title": "Content",
-    "text": " Table of Contents 1. Content 1.1. Example pages 1.2. Other 1. Content Sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. Aenean pulvinar sapien ex, id ultrices lacus consectetur ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et euismod est. Integer eleifend ornare accumsan. Etiam non nulla ut urna suscipit bibendum sed sed urna. 1.1. Example pages AsciiDoc content AsciiDoctor diagrams DrawIo - Digramms.net example 1.2. Other Phasellus lacinia pellentesque odio ac facilisis. Phasellus aliquam Ullamcorper ligula Porttitor ullamcorper. Vestibulum lorem leo, mattis quis ligula eget, vulputate dignissim magna. Nunc dapibus, orci sit amet blandit commodo Note Curabitur in diam quam. Cras eu purus lorem. Phasellus ut blandit ex. Morbi maximus eleifend enim. "
 },
 
 {
@@ -50,26 +50,26 @@ var documents = [
 
 {
     "id": 6,
-    "uri": "20_du/20_asciidoc.html",
-    "menu": "du",
-    "title": "AsciiDoc",
-    "text": " Table of Contents 1. AsciiDoc 1.1. Lists 1.2. Bold, italic, and monospace formatting syntax 1.3. Admonitions 1.4. Tables 1.5. Replacements 1. AsciiDoc 1.1. Lists 1.1.1. Unordered list List item Nested list item Nested list item List item Another nested list item List item 1.1.2. Ordered list Step 1 Step 2 Step 2a Step 2b Step 3 1.1.3. Description list First term The description can be placed on the same line as the term. Second term Description of the second term. The description can also start on its own line. 1.2. Bold, italic, and monospace formatting syntax bold constrained &amp; un constrained italic constrained &amp; un constrained bold italic constrained &amp; un constrained monospace constrained &amp; un constrained monospace bold constrained &amp; un constrained monospace italic constrained &amp; un constrained monospace bold italic constrained &amp; un constrained 1.3. Admonitions Note An admonition draws the reader&#8217;s attention to auxiliary information. Here are the other built-in admonition types: Important Don&#8217;t forget the children! Tip Look for the warp zone under the bridge. Caution Slippery when wet. Warning The software you&#8217;re about to use is untested. Important Sign off before stepping away from your computer. 1.4. Tables Column 1, Header Row Column 2, Header Row Cell in column 1, row 1 Cell in column 2, row 1 Cell in column 1, row 2 Cell in column 2, row 2 1.4.1. With headers Name Group Firefox Web Browser Ruby Programming Language 1.5. Replacements Name Characters Result Copyright (C) &#169; Registered (R) &#174; Registered (R) &#174; Trademark (TM) &#8482; Em dash -- &#8201;&#8212;&#8201; Ellipsis ... &#8230;&#8203; Single right arrow -&gt; &#8594; Double right arrow =&gt; &#8658; Single left arrow &lt;- &#8592; Double left arrow &lt;= &#8656; "
+    "uri": "10_unu/10_intro.html",
+    "menu": "unu",
+    "title": "Intro",
+    "text": " Table of Contents 1. Intro 1. Intro Basic ditaa block: Lorem lacinia pellentesque odio ac facilisis ipsum . "
 },
 
 {
     "id": 7,
-    "uri": "20_du/40_drawio.html",
-    "menu": "du",
-    "title": "Draw.io Example",
-    "text": " Table of Contents 1. Draw.io Example 1. Draw.io Example Pattern [file].dio.png to indicate that the file can be opened by Draw.io / Diagrams.net "
+    "uri": "30_tri/20_advanced/20_topic-B.html",
+    "menu": "tri",
+    "title": "Topic B",
+    "text": " Table of Contents 1. Topic B 1. Topic B Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. "
 },
 
 {
     "id": 8,
-    "uri": "20_du/10_lorem.html",
-    "menu": "du",
-    "title": "Lorem",
-    "text": " Table of Contents 1. Lorem 1. Lorem Lorem diam sed, efficitur massa. Ipsum sed in risus rutrum. Sed lacinia finibus dui et lobortis. Pellentesque et enim. Etiam arcu odio, faucibus non eros nec, mattis tristique mauris. Cras eget aliquet nibh. Curabitur in porta sem, maximus aliquet diam. Integer vitae augue eget ex viverra pharetra. In eu risus at diam lacinia dignissim. Duis vel mauris sed ex lobortis dictum. Nunc sagittis felis et turpis bibendum, id aliquam libero fermentum. Ut dolor nisi, congue nec lorem ac, commodo bibendum nunc. Nam ac felis elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sapien orci, luctus nec ligula nec, ornare hendrerit nunc. Cras vitae consectetur turpis. Phasellus lacinia pellentesque odio ac facilisis Listing 1 . Phasellus aliquam ullamcorper ligula porttitor ullamcorper. Vestibulum lorem leo, mattis quis ligula eget, vulputate dignissim magna. Listing 1. Lorem JSON example { id: 364528, lorem: lorem, data: [9786e2c1, c51147af, 81aa2d6d, 6a5a6b82] } Curabitur in diam quam. Cras eu purus lorem. Phasellus ut blandit ex. Morbi maximus eleifend enim, in venenatis enim auctor pharetra. Sed congue mollis tellus, id molestie libero dictum vitae. Aliquam erat volutpat. Quisque nec blandit nulla. Morbi pretium quis lacus sed fermentum. Donec convallis quam non odio maximus, sed mollis lacus mollis. Sed vehicula auctor magna at dignissim. Donec quis urna et est placerat malesuada. Donec fringilla magna id orci vulputate, eget venenatis ex posuere. Quisque malesuada et mi non finibus. Proin sit amet viverra lorem. Nulla ipsum eros, aliquam id felis sed, malesuada faucibus nisi. Nam pulvinar, sem non tristique scelerisque, mi orci tincidunt justo, quis interdum odio libero at lorem. In luctus iaculis risus, sit amet aliquet metus rutrum molestie. Fusce nec aliquet tellus. Aliquam tincidunt purus dictum quam sodales tempor. Quisque tincidunt purus sem, quis efficitur justo pharetra non. "
+    "uri": "10_unu/20_content.html",
+    "menu": "unu",
+    "title": "Content",
+    "text": " Table of Contents 1. Content 1.1. Example pages 1.2. Other 1. Content Sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. Aenean pulvinar sapien ex, id ultrices lacus consectetur ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et euismod est. Integer eleifend ornare accumsan. Etiam non nulla ut urna suscipit bibendum sed sed urna. 1.1. Example pages AsciiDoc content AsciiDoctor diagrams DrawIo - Digramms.net example 1.2. Other Phasellus lacinia pellentesque odio ac facilisis. Phasellus aliquam Ullamcorper ligula Porttitor ullamcorper. Vestibulum lorem leo, mattis quis ligula eget, vulputate dignissim magna. Nunc dapibus, orci sit amet blandit commodo Note Curabitur in diam quam. Cras eu purus lorem. Phasellus ut blandit ex. Morbi maximus eleifend enim. "
 },
 
 {
