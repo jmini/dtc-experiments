4a3b9780cb513c02e2db97d6e1bef39b7672fc95
jmini <jmini@users.noreply.github.com>
2022-09-09 10:24:13 +0000

    deploy: 63210b2f324face3be16458c342641772592d931

--- lunrjsindex.js
+++ lunrjsindex.js
@@ -18,14 +18,6 @@ var documents = [
 
 {
     "id": 2,
-    "uri": "20_du/10_lorem.html",
-    "menu": "du",
-    "title": "Lorem",
-    "text": " Table of Contents 1. Lorem 1. Lorem Lorem diam sed, efficitur massa. Ipsum sed in risus rutrum. Sed lacinia finibus dui et lobortis. Pellentesque et enim. Etiam arcu odio, faucibus non eros nec, mattis tristique mauris. Cras eget aliquet nibh. Curabitur in porta sem, maximus aliquet diam. Integer vitae augue eget ex viverra pharetra. In eu risus at diam lacinia dignissim. Duis vel mauris sed ex lobortis dictum. Nunc sagittis felis et turpis bibendum, id aliquam libero fermentum. Ut dolor nisi, congue nec lorem ac, commodo bibendum nunc. Nam ac felis elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sapien orci, luctus nec ligula nec, ornare hendrerit nunc. Cras vitae consectetur turpis. Phasellus lacinia pellentesque odio ac facilisis Listing 1 . Phasellus aliquam ullamcorper ligula porttitor ullamcorper. Vestibulum lorem leo, mattis quis ligula eget, vulputate dignissim magna. Listing 1. Lorem JSON example { id: 364528, lorem: lorem, data: [9786e2c1, c51147af, 81aa2d6d, 6a5a6b82] } Curabitur in diam quam. Cras eu purus lorem. Phasellus ut blandit ex. Morbi maximus eleifend enim, in venenatis enim auctor pharetra. Sed congue mollis tellus, id molestie libero dictum vitae. Aliquam erat volutpat. Quisque nec blandit nulla. Morbi pretium quis lacus sed fermentum. Donec convallis quam non odio maximus, sed mollis lacus mollis. Sed vehicula auctor magna at dignissim. Donec quis urna et est placerat malesuada. Donec fringilla magna id orci vulputate, eget venenatis ex posuere. Quisque malesuada et mi non finibus. Proin sit amet viverra lorem. Nulla ipsum eros, aliquam id felis sed, malesuada faucibus nisi. Nam pulvinar, sem non tristique scelerisque, mi orci tincidunt justo, quis interdum odio libero at lorem. In luctus iaculis risus, sit amet aliquet metus rutrum molestie. Fusce nec aliquet tellus. Aliquam tincidunt purus dictum quam sodales tempor. Quisque tincidunt purus sem, quis efficitur justo pharetra non. "
-},
-
-{
-    "id": 3,
     "uri": "30_tri/10_ipsum.html",
     "menu": "tri",
     "title": "Ipsum",
@@ -33,7 +25,7 @@ var documents = [
 },
 
 {
-    "id": 4,
+    "id": 3,
     "uri": "20_du/30_diagrams.html",
     "menu": "du",
     "title": "Asciidoctor Diagrams",
@@ -41,15 +33,15 @@ var documents = [
 },
 
 {
-    "id": 5,
-    "uri": "10_unu/10_intro.html",
-    "menu": "unu",
-    "title": "Intro",
-    "text": " Table of Contents 1. Intro 1. Intro Basic ditaa block: Lorem lacinia pellentesque odio ac facilisis ipsum . "
+    "id": 4,
+    "uri": "20_du/10_lorem.html",
+    "menu": "du",
+    "title": "Lorem",
+    "text": " Table of Contents 1. Lorem 1. Lorem Lorem diam sed, efficitur massa. Ipsum sed in risus rutrum. Sed lacinia finibus dui et lobortis. Pellentesque et enim. Etiam arcu odio, faucibus non eros nec, mattis tristique mauris. Cras eget aliquet nibh. Curabitur in porta sem, maximus aliquet diam. Integer vitae augue eget ex viverra pharetra. In eu risus at diam lacinia dignissim. Duis vel mauris sed ex lobortis dictum. Nunc sagittis felis et turpis bibendum, id aliquam libero fermentum. Ut dolor nisi, congue nec lorem ac, commodo bibendum nunc. Nam ac felis elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sapien orci, luctus nec ligula nec, ornare hendrerit nunc. Cras vitae consectetur turpis. Phasellus lacinia pellentesque odio ac facilisis Listing 1 . Phasellus aliquam ullamcorper ligula porttitor ullamcorper. Vestibulum lorem leo, mattis quis ligula eget, vulputate dignissim magna. Listing 1. Lorem JSON example { id: 364528, lorem: lorem, data: [9786e2c1, c51147af, 81aa2d6d, 6a5a6b82] } Curabitur in diam quam. Cras eu purus lorem. Phasellus ut blandit ex. Morbi maximus eleifend enim, in venenatis enim auctor pharetra. Sed congue mollis tellus, id molestie libero dictum vitae. Aliquam erat volutpat. Quisque nec blandit nulla. Morbi pretium quis lacus sed fermentum. Donec convallis quam non odio maximus, sed mollis lacus mollis. Sed vehicula auctor magna at dignissim. Donec quis urna et est placerat malesuada. Donec fringilla magna id orci vulputate, eget venenatis ex posuere. Quisque malesuada et mi non finibus. Proin sit amet viverra lorem. Nulla ipsum eros, aliquam id felis sed, malesuada faucibus nisi. Nam pulvinar, sem non tristique scelerisque, mi orci tincidunt justo, quis interdum odio libero at lorem. In luctus iaculis risus, sit amet aliquet metus rutrum molestie. Fusce nec aliquet tellus. Aliquam tincidunt purus dictum quam sodales tempor. Quisque tincidunt purus sem, quis efficitur justo pharetra non. "
 },
 
 {
-    "id": 6,
+    "id": 5,
     "uri": "30_tri/20_advanced/20_topic-B.html",
     "menu": "tri",
     "title": "Topic B",
@@ -57,11 +49,19 @@ var documents = [
 },
 
 {
-    "id": 7,
+    "id": 6,
     "uri": "30_tri/20_advanced/10_topic-A.html",
     "menu": "tri",
     "title": "Topic A",
     "text": " Table of Contents 1. Topic A 1. Topic A Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. "
+},
+
+{
+    "id": 7,
+    "uri": "10_unu/10_intro.html",
+    "menu": "unu",
+    "title": "Intro",
+    "text": " Table of Contents 1. Intro 1. Intro Basic ditaa block: Lorem lacinia pellentesque odio ac facilisis ipsum . "
 },
 
 {
