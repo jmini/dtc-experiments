bc365dc58819d6ad0db4286383da4b30b20e2439
jmini <jmini@users.noreply.github.com>
2022-10-26 11:45:03 +0000

    deploy: 4e238a75de4ce985e1053f82c239bf17f2bf0093

--- lunrjsindex.js
+++ lunrjsindex.js
@@ -2,38 +2,6 @@ var documents = [
 
 {
     "id": 0,
-    "uri": "30_tri/20_advanced/20_topic-B.html",
-    "menu": "tri",
-    "title": "Topic B",
-    "text": " Table of Contents 1. Topic B 1. Topic B Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. "
-},
-
-{
-    "id": 1,
-    "uri": "30_tri/20_advanced/10_topic-A.html",
-    "menu": "tri",
-    "title": "Topic A",
-    "text": " Table of Contents 1. Topic A 1. Topic A Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. "
-},
-
-{
-    "id": 2,
-    "uri": "30_tri/10_ipsum.html",
-    "menu": "tri",
-    "title": "Ipsum",
-    "text": " Table of Contents 1. Ipsum 1. Ipsum Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. Aenean pulvinar sapien ex, id ultrices lacus consectetur ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et euismod est. Integer eleifend ornare accumsan. Etiam non nulla ut urna suscipit bibendum sed sed urna. Aenean maximus dictum venenatis. Nullam ultrices lorem sed rutrum egestas. Duis id felis at ex congue venenatis at non turpis. Praesent quis molestie justo. Nam a elementum elit. Nunc vehicula fermentum euismod. Etiam fermentum et purus sed interdum. Phasellus aliquam ullamcorper ligula porttitor ullamcorper. Vestibulum lorem leo, mattis quis ligula eget, vulputate dignissim magna. Listing 1. Ipsum properties example ipsum.enabled=true ipsum.name=ipsum ipsum.id=12598431 Morbi pretium quis lacus sed fermentum. Donec convallis quam non odio maximus, sed mollis lacus mollis. Sed vehicula auctor magna at dignissim. Donec quis urna et est placerat malesuada. Donec fringilla magna id orci vulputate, eget venenatis ex posuere. Quisque malesuada et mi non finibus lorem . Curabitur in diam quam. Cras eu purus lorem. Phasellus ut blandit ex. Morbi maximus eleifend enim, in venenatis enim auctor pharetra. Sed congue mollis tellus, id molestie libero dictum vitae. Aliquam erat volutpat. Quisque nec blandit nulla. "
-},
-
-{
-    "id": 3,
-    "uri": "ara/index.html",
-    "menu": "ara",
-    "title": "Ara Index",
-    "text": " Table of Contents 1. Ara Index 1. Ara Index lorem I have created issue JENKINS-57098 to discuss it. "
-},
-
-{
-    "id": 4,
     "uri": "ara/b.html",
     "menu": "ara",
     "title": "Ara B",
@@ -41,7 +9,7 @@ var documents = [
 },
 
 {
-    "id": 5,
+    "id": 1,
     "uri": "ara/a.html",
     "menu": "ara",
     "title": "Ara A",
@@ -49,11 +17,43 @@ var documents = [
 },
 
 {
-    "id": 6,
+    "id": 2,
+    "uri": "30_tri/20_advanced/20_topic-B.html",
+    "menu": "tri",
+    "title": "Topic B",
+    "text": " Table of Contents 1. Topic B 1. Topic B Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. "
+},
+
+{
+    "id": 3,
+    "uri": "30_tri/20_advanced/10_topic-A.html",
+    "menu": "tri",
+    "title": "Topic A",
+    "text": " Table of Contents 1. Topic A 1. Topic A Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. "
+},
+
+{
+    "id": 4,
+    "uri": "30_tri/10_ipsum.html",
+    "menu": "tri",
+    "title": "Ipsum",
+    "text": " Table of Contents 1. Ipsum 1. Ipsum Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. Aenean pulvinar sapien ex, id ultrices lacus consectetur ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et euismod est. Integer eleifend ornare accumsan. Etiam non nulla ut urna suscipit bibendum sed sed urna. Aenean maximus dictum venenatis. Nullam ultrices lorem sed rutrum egestas. Duis id felis at ex congue venenatis at non turpis. Praesent quis molestie justo. Nam a elementum elit. Nunc vehicula fermentum euismod. Etiam fermentum et purus sed interdum. Phasellus aliquam ullamcorper ligula porttitor ullamcorper. Vestibulum lorem leo, mattis quis ligula eget, vulputate dignissim magna. Listing 1. Ipsum properties example ipsum.enabled=true ipsum.name=ipsum ipsum.id=12598431 Morbi pretium quis lacus sed fermentum. Donec convallis quam non odio maximus, sed mollis lacus mollis. Sed vehicula auctor magna at dignissim. Donec quis urna et est placerat malesuada. Donec fringilla magna id orci vulputate, eget venenatis ex posuere. Quisque malesuada et mi non finibus lorem . Curabitur in diam quam. Cras eu purus lorem. Phasellus ut blandit ex. Morbi maximus eleifend enim, in venenatis enim auctor pharetra. Sed congue mollis tellus, id molestie libero dictum vitae. Aliquam erat volutpat. Quisque nec blandit nulla. "
+},
+
+{
+    "id": 5,
     "uri": "10_unu/10_intro.html",
     "menu": "unu",
     "title": "Intro",
     "text": " Table of Contents 1. Intro 1. Intro Basic ditaa block: Lorem lacinia pellentesque odio ac facilisis ipsum . "
+},
+
+{
+    "id": 6,
+    "uri": "ara/index.html",
+    "menu": "ara",
+    "title": "Ara Index",
+    "text": " Table of Contents 1. Ara Index 1. Ara Index lorem I have created issue JENKINS-57098 to discuss it. "
 },
 
 {
@@ -74,18 +74,18 @@ var documents = [
 
 {
     "id": 9,
-    "uri": "20_du/20_asciidoc.html",
-    "menu": "du",
-    "title": "AsciiDoc",
-    "text": " Table of Contents 1. AsciiDoc 1.1. Lists 1.2. Bold, italic, and monospace formatting syntax 1.3. Admonitions 1.4. Tables 1.5. Replacements 1. AsciiDoc 1.1. Lists 1.1.1. Unordered list List item Nested list item Nested list item List item Another nested list item List item 1.1.2. Ordered list Step 1 Step 2 Step 2a Step 2b Step 3 1.1.3. Description list First term The description can be placed on the same line as the term. Second term Description of the second term. The description can also start on its own line. 1.2. Bold, italic, and monospace formatting syntax bold constrained &amp; un constrained italic constrained &amp; un constrained bold italic constrained &amp; un constrained monospace constrained &amp; un constrained monospace bold constrained &amp; un constrained monospace italic constrained &amp; un constrained monospace bold italic constrained &amp; un constrained 1.3. Admonitions Note An admonition draws the reader&#8217;s attention to auxiliary information. Here are the other built-in admonition types: Important Don&#8217;t forget the children! Tip Look for the warp zone under the bridge. Caution Slippery when wet. Warning The software you&#8217;re about to use is untested. Important Sign off before stepping away from your computer. 1.4. Tables Column 1, Header Row Column 2, Header Row Cell in column 1, row 1 Cell in column 2, row 1 Cell in column 1, row 2 Cell in column 2, row 2 1.4.1. With headers Name Group Firefox Web Browser Ruby Programming Language 1.5. Replacements Name Characters Result Copyright (C) &#169; Registered (R) &#174; Registered (R) &#174; Trademark (TM) &#8482; Em dash -- &#8201;&#8212;&#8201; Ellipsis ... &#8230;&#8203; Single right arrow -&gt; &#8594; Double right arrow =&gt; &#8658; Single left arrow &lt;- &#8592; Double left arrow &lt;= &#8656; "
-},
-
-{
-    "id": 10,
     "uri": "20_du/30_diagrams.html",
     "menu": "du",
     "title": "Asciidoctor Diagrams",
     "text": " Table of Contents 1. Asciidoctor Diagrams 1.1. ditaa 1.2. graphviz 1.3. plantuml 1. Asciidoctor Diagrams 1.1. ditaa 1.2. graphviz 1.3. plantuml "
+},
+
+{
+    "id": 10,
+    "uri": "20_du/20_asciidoc.html",
+    "menu": "du",
+    "title": "AsciiDoc",
+    "text": " Table of Contents 1. AsciiDoc 1.1. Lists 1.2. Bold, italic, and monospace formatting syntax 1.3. Admonitions 1.4. Tables 1.5. Replacements 1. AsciiDoc 1.1. Lists 1.1.1. Unordered list List item Nested list item Nested list item List item Another nested list item List item 1.1.2. Ordered list Step 1 Step 2 Step 2a Step 2b Step 3 1.1.3. Description list First term The description can be placed on the same line as the term. Second term Description of the second term. The description can also start on its own line. 1.2. Bold, italic, and monospace formatting syntax bold constrained &amp; un constrained italic constrained &amp; un constrained bold italic constrained &amp; un constrained monospace constrained &amp; un constrained monospace bold constrained &amp; un constrained monospace italic constrained &amp; un constrained monospace bold italic constrained &amp; un constrained 1.3. Admonitions Note An admonition draws the reader&#8217;s attention to auxiliary information. Here are the other built-in admonition types: Important Don&#8217;t forget the children! Tip Look for the warp zone under the bridge. Caution Slippery when wet. Warning The software you&#8217;re about to use is untested. Important Sign off before stepping away from your computer. 1.4. Tables Column 1, Header Row Column 2, Header Row Cell in column 1, row 1 Cell in column 2, row 1 Cell in column 1, row 2 Cell in column 2, row 2 1.4.1. With headers Name Group Firefox Web Browser Ruby Programming Language 1.5. Replacements Name Characters Result Copyright (C) &#169; Registered (R) &#174; Registered (R) &#174; Trademark (TM) &#8482; Em dash -- &#8201;&#8212;&#8201; Ellipsis ... &#8230;&#8203; Single right arrow -&gt; &#8594; Double right arrow =&gt; &#8658; Single left arrow &lt;- &#8592; Double left arrow &lt;= &#8656; "
 },
 
 {
