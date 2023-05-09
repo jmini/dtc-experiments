0c6991358923ea18cb6fe449bcfd4fe7aecf786b
jmini <jmini@users.noreply.github.com>
2023-05-09 14:46:33 +0000

    deploy: 214c8d9a87586a4ba1b4f41e328fb338c3fb3ebf

--- lunrjsindex.js
+++ lunrjsindex.js
@@ -2,54 +2,6 @@ var documents = [
 
 {
     "id": 0,
-    "uri": "ara/b.html",
-    "menu": "ara",
-    "title": "Ara B",
-    "text": " Table of Contents 1. Ara B 1. Ara B lorem "
-},
-
-{
-    "id": 1,
-    "uri": "ara/a.html",
-    "menu": "ara",
-    "title": "Ara A",
-    "text": " Table of Contents 1. Ara A 1. Ara A lorem "
-},
-
-{
-    "id": 2,
-    "uri": "30_tri/20_advanced/20_topic-B.html",
-    "menu": "tri",
-    "title": "Topic B",
-    "text": " Table of Contents 1. Topic B 1. Topic B Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. "
-},
-
-{
-    "id": 3,
-    "uri": "30_tri/20_advanced/10_topic-A.html",
-    "menu": "tri",
-    "title": "Topic A",
-    "text": " Table of Contents 1. Topic A 1. Topic A Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. "
-},
-
-{
-    "id": 4,
-    "uri": "30_tri/10_ipsum.html",
-    "menu": "tri",
-    "title": "Ipsum",
-    "text": " Table of Contents 1. Ipsum 1. Ipsum Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. Aenean pulvinar sapien ex, id ultrices lacus consectetur ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et euismod est. Integer eleifend ornare accumsan. Etiam non nulla ut urna suscipit bibendum sed sed urna. Aenean maximus dictum venenatis. Nullam ultrices lorem sed rutrum egestas. Duis id felis at ex congue venenatis at non turpis. Praesent quis molestie justo. Nam a elementum elit. Nunc vehicula fermentum euismod. Etiam fermentum et purus sed interdum. Phasellus aliquam ullamcorper ligula porttitor ullamcorper. Vestibulum lorem leo, mattis quis ligula eget, vulputate dignissim magna. Listing 1. Ipsum properties example ipsum.enabled=true ipsum.name=ipsum ipsum.id=12598431 Morbi pretium quis lacus sed fermentum. Donec convallis quam non odio maximus, sed mollis lacus mollis. Sed vehicula auctor magna at dignissim. Donec quis urna et est placerat malesuada. Donec fringilla magna id orci vulputate, eget venenatis ex posuere. Quisque malesuada et mi non finibus lorem . Curabitur in diam quam. Cras eu purus lorem. Phasellus ut blandit ex. Morbi maximus eleifend enim, in venenatis enim auctor pharetra. Sed congue mollis tellus, id molestie libero dictum vitae. Aliquam erat volutpat. Quisque nec blandit nulla. "
-},
-
-{
-    "id": 5,
-    "uri": "10_unu/10_intro.html",
-    "menu": "unu",
-    "title": "Intro",
-    "text": " Table of Contents 1. Intro 1. Intro Basic ditaa block: Lorem lacinia pellentesque odio ac facilisis ipsum . "
-},
-
-{
-    "id": 6,
     "uri": "ara/index.html",
     "menu": "ara",
     "title": "Ara Index",
@@ -57,7 +9,31 @@ var documents = [
 },
 
 {
-    "id": 7,
+    "id": 1,
+    "uri": "ara/b.html",
+    "menu": "ara",
+    "title": "Ara B",
+    "text": " Table of Contents 1. Ara B 1. Ara B lorem "
+},
+
+{
+    "id": 2,
+    "uri": "ara/a.html",
+    "menu": "ara",
+    "title": "Ara A",
+    "text": " Table of Contents 1. Ara A 1. Ara A lorem "
+},
+
+{
+    "id": 3,
+    "uri": "10_unu/10_intro.html",
+    "menu": "unu",
+    "title": "Intro",
+    "text": " Table of Contents 1. Intro 1. Intro Basic ditaa block: Lorem lacinia pellentesque odio ac facilisis ipsum . "
+},
+
+{
+    "id": 4,
     "uri": "10_unu/20_content.html",
     "menu": "unu",
     "title": "Content",
@@ -65,23 +41,7 @@ var documents = [
 },
 
 {
-    "id": 8,
-    "uri": "20_du/40_drawio.html",
-    "menu": "du",
-    "title": "Draw.io Example",
-    "text": " Table of Contents 1. Draw.io Example 1. Draw.io Example Pattern [file].dio.png to indicate that the file can be opened by Draw.io / Diagrams.net "
-},
-
-{
-    "id": 9,
-    "uri": "20_du/30_diagrams.html",
-    "menu": "du",
-    "title": "Asciidoctor Diagrams",
-    "text": " Table of Contents 1. Asciidoctor Diagrams 1.1. ditaa 1.2. graphviz 1.3. plantuml 1. Asciidoctor Diagrams 1.1. ditaa 1.2. graphviz 1.3. plantuml "
-},
-
-{
-    "id": 10,
+    "id": 5,
     "uri": "20_du/20_asciidoc.html",
     "menu": "du",
     "title": "AsciiDoc",
@@ -89,11 +49,51 @@ var documents = [
 },
 
 {
-    "id": 11,
+    "id": 6,
+    "uri": "20_du/40_drawio.html",
+    "menu": "du",
+    "title": "Draw.io Example",
+    "text": " Table of Contents 1. Draw.io Example 1. Draw.io Example Pattern [file].dio.png to indicate that the file can be opened by Draw.io / Diagrams.net "
+},
+
+{
+    "id": 7,
+    "uri": "20_du/30_diagrams.html",
+    "menu": "du",
+    "title": "Asciidoctor Diagrams",
+    "text": " Table of Contents 1. Asciidoctor Diagrams 1.1. ditaa 1.2. graphviz 1.3. plantuml 1. Asciidoctor Diagrams 1.1. ditaa 1.2. graphviz 1.3. plantuml "
+},
+
+{
+    "id": 8,
     "uri": "20_du/10_lorem.html",
     "menu": "du",
     "title": "Lorem",
     "text": " Table of Contents 1. Lorem 1. Lorem Lorem diam sed, efficitur massa. Ipsum sed in risus rutrum. Sed lacinia finibus dui et lobortis. Pellentesque et enim. Etiam arcu odio, faucibus non eros nec, mattis tristique mauris. Cras eget aliquet nibh. Curabitur in porta sem, maximus aliquet diam. Integer vitae augue eget ex viverra pharetra. In eu risus at diam lacinia dignissim. Duis vel mauris sed ex lobortis dictum. Nunc sagittis felis et turpis bibendum, id aliquam libero fermentum. Ut dolor nisi, congue nec lorem ac, commodo bibendum nunc. Nam ac felis elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sapien orci, luctus nec ligula nec, ornare hendrerit nunc. Cras vitae consectetur turpis. Phasellus lacinia pellentesque odio ac facilisis Listing 1 . Phasellus aliquam ullamcorper ligula porttitor ullamcorper. Vestibulum lorem leo, mattis quis ligula eget, vulputate dignissim magna. Listing 1. Lorem JSON example { id: 364528, lorem: lorem, data: [9786e2c1, c51147af, 81aa2d6d, 6a5a6b82] } Curabitur in diam quam. Cras eu purus lorem. Phasellus ut blandit ex. Morbi maximus eleifend enim, in venenatis enim auctor pharetra. Sed congue mollis tellus, id molestie libero dictum vitae. Aliquam erat volutpat. Quisque nec blandit nulla. Morbi pretium quis lacus sed fermentum. Donec convallis quam non odio maximus, sed mollis lacus mollis. Sed vehicula auctor magna at dignissim. Donec quis urna et est placerat malesuada. Donec fringilla magna id orci vulputate, eget venenatis ex posuere. Quisque malesuada et mi non finibus. Proin sit amet viverra lorem. Nulla ipsum eros, aliquam id felis sed, malesuada faucibus nisi. Nam pulvinar, sem non tristique scelerisque, mi orci tincidunt justo, quis interdum odio libero at lorem. In luctus iaculis risus, sit amet aliquet metus rutrum molestie. Fusce nec aliquet tellus. Aliquam tincidunt purus dictum quam sodales tempor. Quisque tincidunt purus sem, quis efficitur justo pharetra non. "
+},
+
+{
+    "id": 9,
+    "uri": "30_tri/20_advanced/20_topic-B.html",
+    "menu": "tri",
+    "title": "Topic B",
+    "text": " Table of Contents 1. Topic B 1. Topic B Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. "
+},
+
+{
+    "id": 10,
+    "uri": "30_tri/20_advanced/10_topic-A.html",
+    "menu": "tri",
+    "title": "Topic A",
+    "text": " Table of Contents 1. Topic A 1. Topic A Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. "
+},
+
+{
+    "id": 11,
+    "uri": "30_tri/10_ipsum.html",
+    "menu": "tri",
+    "title": "Ipsum",
+    "text": " Table of Contents 1. Ipsum 1. Ipsum Ipsum sed in risus rutrum, commodo quam nec, viverra nulla. Curabitur porta tincidunt urna. Aenean pulvinar sapien ex, id ultrices lacus consectetur ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et euismod est. Integer eleifend ornare accumsan. Etiam non nulla ut urna suscipit bibendum sed sed urna. Aenean maximus dictum venenatis. Nullam ultrices lorem sed rutrum egestas. Duis id felis at ex congue venenatis at non turpis. Praesent quis molestie justo. Nam a elementum elit. Nunc vehicula fermentum euismod. Etiam fermentum et purus sed interdum. Phasellus aliquam ullamcorper ligula porttitor ullamcorper. Vestibulum lorem leo, mattis quis ligula eget, vulputate dignissim magna. Listing 1. Ipsum properties example ipsum.enabled=true ipsum.name=ipsum ipsum.id=12598431 Morbi pretium quis lacus sed fermentum. Donec convallis quam non odio maximus, sed mollis lacus mollis. Sed vehicula auctor magna at dignissim. Donec quis urna et est placerat malesuada. Donec fringilla magna id orci vulputate, eget venenatis ex posuere. Quisque malesuada et mi non finibus lorem . Curabitur in diam quam. Cras eu purus lorem. Phasellus ut blandit ex. Morbi maximus eleifend enim, in venenatis enim auctor pharetra. Sed congue mollis tellus, id molestie libero dictum vitae. Aliquam erat volutpat. Quisque nec blandit nulla. "
 },
 
 {
